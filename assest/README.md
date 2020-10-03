e4801c59ec0e2cc33aecdebd2739bb61f2c28849
amirholyanasabnarab <70652353+amirholyanasabnarab@users.noreply.github.com>
2020-10-03 16:27:50 +0330

    990712

---
+++ assest/README.md
@@ -0,0 +1,4 @@
+BREAKPOINT CSS 2020
+Mobile => 688 px
+Tablet => 992 px
+Desktop =>1312 px
---
+++ assest/js/script.js
@@ -0,0 +1,11 @@
+let isClose=true;
+function onToggle() {
+    const menu = document.getElementById('menu');
+    if (isClose) {
+        menu.classList.add('active');
+        isClose = false;
+    } else {
+        menu.classList.remove('active');
+        isClose = true;
+    }
+}
